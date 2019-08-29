9aec5789fe2b11853c53cd703f072f7eb7e8475f
Apizaco <jesus.rodrigu20@anahuac.mx>
2019-08-28 21:11:09 -0500

    10

--- www/js/app.js
+++ www/js/app.js
@@ -696,7 +696,7 @@ function ConvertPDF(){
 var content = '<html><h1>Infracción</h1>';
     content +='<p>Fecha: '+_fecha+'</p>';
     content +='<p>Folio: '+_folio+'</p>';
-    content +='<p><img src="'+_img+'" /></p>';
+    content +='<p><img src="'+_img+'" width="100" height="300" /></p>';
     content +='</html>';
 
 pdf.fromData( content, options)
